fe8979dbc43998b7ca252e8262ed16bc26c56505
hosalt <hosalt@qq.com>
2018-03-12 22:22:52 +0800

    打包优化

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
+const ExtractPlugin = require('extract-text-webpack-plugin')
+
 const isDev = process.env.NODE_ENV === 'development'
 
 
@@ -9,7 +11,7 @@ const config = {
   entry: path.join(__dirname,'src/index.js'),
   //entry: path.resolve(__dirname,'src/index.js'),
   output:{
-    filename: 'bundle.js',
+    filename: 'bundle.[hash:8].js',
     path: path.join(__dirname,'dist')
   },
   module:{
@@ -22,7 +24,7 @@ const config = {
         test:/\.jsx$/,
         loader: 'babel-loader'
       },
-      {
+      /*{
         test:/\.css$/,
         //loader: 'css-loader'
         use:[
@@ -43,7 +45,7 @@ const config = {
           },
           'stylus-loader'
         ]
-      },
+      },*/
       {
         test: /\.(gif|jpe?g|png|svg)(\?.+)?$/,
         use:[
@@ -69,6 +71,20 @@ const config = {
 }
 
 if(isDev){
+  config.module.rules.push({
+    test: /\.styl/,
+    use:[
+      'style-loader',
+      'css-loader',
+      {
+        loader:'postcss-loader',
+        options:{
+          sourceMap: true
+        }
+      },
+      'stylus-loader'
+    ]
+  })
   config.devtool = '#cheap-module-eval-source-map'
   config.devServer = {
     port: 8000,
@@ -85,6 +101,37 @@ if(isDev){
   config.plugins.push(
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin
+  )
+}else{
+  config.entry = {
+    app: path.join(__dirname,'src/index.js'),
+    vendor: ['vue']
+  }
+  config.output.filename = '[name].[chunkhash:8].js'
+  config.module.rules.push({
+    test: /\.styl/,
+    use: ExtractPlugin.extract({
+      fallback: 'style-loader',
+      use:[
+        'css-loader',
+        {
+          loader:'postcss-loader',
+          options:{
+            sourceMap: true
+          }
+        },
+        'stylus-loader'
+      ]
+    })
+  })
+  config.plugins.push(
+    new ExtractPlugin('styles.[contentHash:8].css'),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'runtime',
+    })
   )
 }
 
